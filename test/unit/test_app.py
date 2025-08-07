1068766c18a08f3b250a3a17c792b222e762368a
Chris Taylor <chris11.taylor@nttdata.com>
2025-08-07 14:53:14 -0500

    Fix tests

--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -6,14 +6,11 @@ from function import app
 def test_primary_handler():
     result = app.lambda_handler(None, None)
     assert result["statusCode"] == 200
-    assert result["body"] == "Hello from a sample Python Lambda Function! Payload: None"
+    assert "Payload: None" in result["body"]
 
 
 def test_primary_handler_with_payload():
     with patch.object(app, "PAYLOAD", "test_payload"):
         result = app.lambda_handler(None, None)
         assert result["statusCode"] == 200
-        assert (
-            result["body"]
-            == "Hello from a sample Python Lambda Function! Payload: test_payload"
-        )
+        assert "test_payload" in result["body"]
