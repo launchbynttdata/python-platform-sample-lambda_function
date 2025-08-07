a57e2225bb28a769f50ae9c88c84c817950d32be
Chris Taylor <chris11.taylor@nttdata.com>
2025-08-07 14:48:33 -0500

    Update body text

--- src/function/app.py
+++ src/function/app.py
@@ -6,5 +6,5 @@ PAYLOAD = os.environ.get("PAYLOAD")
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": f"Hello from a sample Python Lambda Function! Payload: {PAYLOAD}",
+        "body": f"Hello from an ephemeral Python Lambda Function! Payload: {PAYLOAD}",
     }
